585815f28b85aa11910a72eb32c34884d6d2d0c6
Ruslan Mikhalev <ruslan.mikhalev@2degrees.nz>
2023-06-21 18:59:45 +1200

    Fixed cards controllers

--- controllers/cards.js
+++ controllers/cards.js
@@ -8,11 +8,12 @@ const {
 // 500 - Ошибка по умолчанию.
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .populate('user')
+    .populate(['owner', 'likes'])
     .then((cards) => {
       res.send(cards);
     })
     .catch((err) => {
+      console.log(`err: ${err}`);
       res.status(INTERNAL_SERVER_ERROR).send(errorBody(DEFAULT_ERROR_MESSAGE));
     });
 };
@@ -21,11 +22,11 @@ module.exports.getCards = (req, res) => {
 // 500 - Ошибка по умолчанию.
 module.exports.postCard = (req, res) => {
   const {
-    name, link, owner, likes, createdAt,
+    name, link, likes = [],
   } = req.body;
 
   Card.create({
-    name, link, owner, likes, createdAt,
+    name, link, owner: req.user._id, likes,
   })
     .then((card) => {
       res.send(card);
@@ -39,7 +40,7 @@ module.exports.postCard = (req, res) => {
 // 500 - Ошибка по умолчанию.
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .populate('user')
+    .populate(['owner', 'likes'])
     .then((card) => res.send(card))
     .catch((err) => {
       res.status(INTERNAL_SERVER_ERROR).send(errorBody(DEFAULT_ERROR_MESSAGE));
@@ -50,14 +51,16 @@ module.exports.deleteCard = (req, res) => {
 // 404 - Передан несуществующий _id карточки.
 // 500 - Ошибка по умолчанию.
 module.exports.likeCard = (req, res) => {
+  console.log(`likeCard cardId: ${req.params.cardId}`);
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true, runValidators: true },
   )
-    .populate('user')
+    .populate(['owner', 'likes'])
     .then((card) => res.send(card))
     .catch((err) => {
+      console.log(`err: ${err}`);
       res.status(INTERNAL_SERVER_ERROR).send(errorBody(DEFAULT_ERROR_MESSAGE));
     });
 };
@@ -66,16 +69,18 @@ module.exports.likeCard = (req, res) => {
 // 404 - Передан несуществующий _id карточки.
 // 500 - Ошибка по умолчанию.
 module.exports.unlikeCard = (req, res) => {
+  console.log(`unlikeCard cardId: ${req.params.cardId}`);
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
     { new: true, runValidators: true },
   )
-    .populate('user')
+    .populate(['owner', 'likes'])
     .then((card) => {
       res.send(card);
     })
     .catch((err) => {
+      console.log(`err: ${err}`);
       res.status(INTERNAL_SERVER_ERROR).send(errorBody(DEFAULT_ERROR_MESSAGE));
     });
 };
