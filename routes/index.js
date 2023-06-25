99d57161a15c8f2e7b4044698cb4a9c782de3a2a
Ruslan Mikhalev <ruslan.mikhalev@2degrees.nz>
2023-06-25 15:31:51 +1200

    Wrong path error added

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,13 @@
 const router = require('express').Router();
 const {
-  NOT_FOUND_ERROR,
-  errorBody,
-  NOT_FOUND_ERROR_MESSAGE,
+  NotFoundError,
 } = require('../middlewares/errors');
 
 router.use('/cards', require('./cards'));
 router.use('/users', require('./users'));
 
-router.use((req, res) => {
-  res.status(NOT_FOUND_ERROR).send(errorBody(NOT_FOUND_ERROR_MESSAGE));
+router.use((req, res, next) => {
+  next(new NotFoundError('Неправильный путь'));
 });
 
 module.exports = router;
